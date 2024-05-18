df94d8e49411e4120f841e844b3b58968a7b0332
Natan Balthazar <balthazarnatan@gmail.com>
2024-05-18 15:57:37 -0300

    Creating the application's color themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import GlobalStyles from "./styles/GlobalStyles";
 
 import Layout from "./components/Layout";
+import { ThemeProvider } from "styled-components";
+import dark from "./styles/themes/dark";
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 };
 
