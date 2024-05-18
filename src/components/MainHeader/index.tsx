a7859bedf7f86e314614f60e21f707f66ebdc96d
Natan Balthazar <balthazarnatan@gmail.com>
2024-05-18 16:15:12 -0300

    Criando o header principal - Saudação

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useMemo } from "react";
+import emojis from "../../utils/emojis";
 
-import { Container } from "./styles";
+import { Container, Profile, Welcome, Username } from "./styles";
 
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+    return emojis[indice];
+  }, []);
+
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Togle</h1>
+      <Profile>
+        <Welcome>Olá, {emoji}</Welcome>
+        <Username>Natan Balthazar</Username>
+      </Profile>
     </Container>
   );
 };
--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -2,6 +2,28 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   grid-area: MH;
+  background-color: ${(props) => props.theme.colors.secondary};
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+
+  border-bottom: 1px solid ${(props) => props.theme.colors.gray};
+`;
+
+export const Profile = styled.div`
+  color: ${(props) => props.theme.colors.white};
+`;
+
+export const Welcome = styled.h3`
+  grid-area: MH;
+  color: ${(props) => props.theme.colors.white};
+  background-color: ${(props) => props.theme.colors.secondary};
+`;
+
+export const Username = styled.span`
+  grid-area: MH;
   color: ${(props) => props.theme.colors.white};
   background-color: ${(props) => props.theme.colors.secondary};
 `;
