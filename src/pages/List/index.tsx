4b109ef7b9ccabf0e3f7ad35e4f9f1581e67fc16
Natan Balthazar <balthazarnatan@gmail.com>
2024-05-20 12:12:34 -0300

    Creating the filters

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,26 +1,92 @@
 import React from "react";
-import { Container, Content } from "./styles";
+import { Container, Content, Filters } from "./styles";
 import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 
 const List: React.FC = () => {
-  const options = [
+  const months = [
     {
-      value: "Natan",
-      label: "Natan",
+      value: 1,
+      label: "Janeiro",
     },
     {
-      value: "Teste",
-      label: "Teste",
+      value: 2,
+      label: "Fevereiro",
+    },
+    {
+      value: 3,
+      label: "Março",
+    },
+    {
+      value: 4,
+      label: "Abril",
+    },
+    {
+      value: 5,
+      label: "Maio",
+    },
+    {
+      value: 6,
+      label: "Junho",
+    },
+    {
+      value: 7,
+      label: "Julho",
+    },
+    {
+      value: 8,
+      label: "Agosto",
+    },
+    {
+      value: 9,
+      label: "Setembro",
+    },
+    {
+      value: 10,
+      label: "Outubro",
+    },
+    {
+      value: 11,
+      label: "Novembro",
+    },
+    {
+      value: 12,
+      label: "Dezembro",
+    },
+  ];
+
+  const years = [
+    {
+      value: 2024,
+      label: 2024,
+    },
+    {
+      value: 2023,
+      label: 2023,
+    },
+    {
+      value: 2022,
+      label: 2022,
     },
   ];
 
   return (
     <Container>
       <ContentHeader title="List" lineColor="#fff">
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-recurrent">
+          Recorrentes
+        </button>
+        <button type="button" className="tag-filter tag-filter-eventual">
+          Eventuais
+        </button>
+      </Filters>
+
       <Content>
         <HistoryFinanceCard
           tagColor="red"
